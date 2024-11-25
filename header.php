c68e28036b8d9858f81934fcb0403e85f78ce932
Vinicius-Martins <vinicius_apple22@outlookc.com>
2024-11-25 20:41:44 -0300

    lógica de headers

---
+++ header.php
@@ -0,0 +1,67 @@
+<?php
+
+    if(!isset($_SESSION)) {
+        session_start();
+    }
+
+    function loadHeader () {
+        if(!$_SESSION['login']) {
+            return headerUser();
+        } else {
+            return headerAdmin();
+        }
+    }
+
+    function headerUser() {
+        echo '<nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a href="index.php" class="navbar-brand">Portal de Noticias</a>
+                
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Escrever Noticia</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="login.php">Logar como administrador</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Buscar Noticia" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit"><i class="bi bi-search"></i></button>
+                    </form>
+                </div>
+            </div>
+        </nav>';
+    }
+    
+    function headerAdmin() {
+        echo '<nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a href="index.php" class="navbar-brand">Portal de Noticias</a>
+                
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Escrever Noticia</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Aprovar Noticias</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Logout</a>
+                        </li>
+                        
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Buscar Noticia" aria-label="Search">
+                        <button class="btn btn-outline-success" type="submit"><i class="bi bi-search"></i></button>
+                    </form>
+                </div>
+            </div>
+        </nav>';
+    }
+?>
