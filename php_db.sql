296276dcea13e740736fc1aba1b6957bd059e0f8
vinicius <vinicius_apple22@outlook.com>
2024-12-05 22:17:45 -0300

    adicionando login de escritor

--- php_db.sql
+++ php_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 01/12/2024 às 22:57
+-- Tempo de geração: 06/12/2024 às 02:17
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -22,6 +22,44 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `php_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `php_db`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(3) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `admin`
+--
+
+INSERT INTO `admin` (`id`, `login`, `senha`) VALUES
+(2, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `escritor`
+--
+
+CREATE TABLE `escritor` (
+  `id` int(11) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `escritor`
+--
+
+INSERT INTO `escritor` (`id`, `login`, `senha`) VALUES
+(1, 'escritor', 'escritor');
 
 -- --------------------------------------------------------
 
@@ -49,34 +87,9 @@ INSERT INTO `noticias` (`id`, `titulo`, `descricao`, `corpo`, `imagem`, `publica
 (23, 'Novo Tratamento Promissor Contra o Câncer É Testado com Sucesso', 'Um tratamento experimental apresentou resultados promissores em pacientes com câncer avançado.', 'Pesquisadores da Universidade de Stanford divulgaram um estudo sobre um tratamento que utiliza nanopartículas para atacar células cancerígenas. Durante os testes clínicos, 70% dos pacientes com câncer avançado apresentaram remissão completa. Apesar do otimismo, os especialistas alertam que mais testes serão necessários antes da aprovação definitiva pelas agências de saúde.', '/imagens/noticia3.jpg', '2024-11-30', 0),
 (24, 'Brasil Lidera Ranking de Energia Renovável na América Latina', 'O país ultrapassou metas internacionais para a geração de energia limpa.', 'O Brasil foi reconhecido pela ONU como líder em energia renovável na América Latina. Com mais de 80% de sua matriz energética proveniente de fontes limpas, como hidrelétrica, solar e eólica, o país superou metas estabelecidas no Acordo de Paris. Especialistas acreditam que, com mais investimentos, o Brasil pode se tornar referência global no setor.\r\n\r\n', '/imagens/noticia4.jpg', '2024-11-30', 0);
 
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `admin`
---
-
-CREATE TABLE `admin` (
-  `id` int(3) NOT NULL,
-  `login` varchar(255) NOT NULL,
-  `senha` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `admin`
---
-
-INSERT INTO `admin` (`id`, `login`, `senha`) VALUES
-(2, 'admin', 'admin');
-
 --
 -- Índices para tabelas despejadas
 --
-
---
--- Índices de tabela `noticias`
---
-ALTER TABLE `noticias`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices de tabela `admin`
@@ -85,20 +98,38 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT para tabelas despejadas
+-- Índices de tabela `escritor`
 --
+ALTER TABLE `escritor`
+  ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT de tabela `noticias`
+-- Índices de tabela `noticias`
 --
 ALTER TABLE `noticias`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
 
 --
 -- AUTO_INCREMENT de tabela `admin`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `escritor`
+--
+ALTER TABLE `escritor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `noticias`
+--
+ALTER TABLE `noticias`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
